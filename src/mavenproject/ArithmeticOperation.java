e087a5850cbd5e189561ed6d34640ea2563fedf1
Shravan Kumar Singh <SHRSINGH@amdocs.com>
2019-02-04 11:57:50 +0200

    initial commit

---
+++ src/mavenproject/ArithmeticOperation.java
@@ -0,0 +1,24 @@
+package mavenproject;
+public class ArithmeticOperation {
+
+	public int add(int a,int b)
+	{
+		int c = a+b;
+		return c;
+	}
+	public int subtract(int a, int b)
+	{
+		int c = a-b;
+		return c;
+	}
+	public int multiply(int a, int b)
+	{
+		int c = a*b;
+		return c;
+	}
+	public int divide(int a, int b)
+	{
+		int c = a/b;
+		return c;
+	}
+}
---
+++ src/mavenproject/MavenProject.java
@@ -0,0 +1,15 @@
+package mavenproject;
+
+public class MavenProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Adding Comments
+		ArithmeticOperation op = new ArithmeticOperation();
+		int a = 20;
+		int b = 30;
+		int c = op.multiply(a,b);
+		System.out.printf("Value of Operation = " + c);
+	}
+
+}
